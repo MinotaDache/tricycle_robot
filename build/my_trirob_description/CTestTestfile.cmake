1bbe7db625a7e78a41676e4dae27cf66e31f8eb6
Minota Matewos Dache <minotadache@gmail.com>
2026-03-20 10:18:51 +0100

    Ignore build/install/log and VSCode files

--- build/my_trirob_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/minota/tricycle_robot/src/my_trirob_description
-# Build directory: /home/minota/tricycle_robot/build/my_trirob_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
