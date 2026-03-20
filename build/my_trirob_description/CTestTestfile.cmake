60e1a9e35df0ccee6a4d6574d65707762d41bbb9
Minota Matewos Dache <minotadache@gmail.com>
2026-03-20 09:37:50 +0100

    created urdf package

---
+++ build/my_trirob_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/minota/tricycle_robot/src/my_trirob_description
+# Build directory: /home/minota/tricycle_robot/build/my_trirob_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
